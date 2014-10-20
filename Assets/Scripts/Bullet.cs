e5895a484a4452327d4b2214cbd1944f69e8f35f
daimaruonline <dogihara@gmail.com>
2014-10-21 02:18:04 +0900

    third day 3/4

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.name == "Plane") {
+			Destroy(this.gameObject, 2.5f);
+		} else if (collision.collider.tag == "Teddy") {
+			Destroy(this.gameObject);
+		}
+	}
+}
